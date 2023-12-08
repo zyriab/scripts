2bf346e85e0d9b43b9098575a844dab6a214bf0e
Arthur Wallendorff <hello@metaoist.io>
2023-12-08 23:49:55 +0100

    chore: bye shippr

--- setup-aliases.sh
+++
@@ -1,8 +0,0 @@
-echo "alias spr='cd ~/Developer/shippr'
-alias cap='cd ~/Developer/shippr/projects/carrier-app'" >> ~/.bash_aliases
-
-if [ -f ~/.zshrc ]; then
-echo "if [ -f ~/.bash_aliases ]; then
-. ~/.bash_aliases
-fi" >> ~/.zshrc
-fi
